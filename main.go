3e8d4e94a451174ea4e7ea105ea244a0c6029603
Jeevan Kumar B <jeevan_b1@persistent.com>
2022-12-17 12:08:13 +0530

    print dependency in JSON format

--- main.go
+++ main.go
@@ -1,37 +1,164 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"log"
 	"os"
-	"os/exec"
-
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
+	"strings"
 )
+
+type Pkg struct {
+	name   string
+	childs []string
+}
+
+type PkgTree struct {
+	mapPkg map[string]*Pkg
+	root   string
+	depth  int
+}
+
+type Artifact struct {
+	Name        string //`json:"name"`
+	Version     string //`json:"color"`
+	Dependecies []Artifact
+}
+
+var Arr []Artifact
+var count int
+
+func NewPkgTree(depth int) *PkgTree {
+	return &PkgTree{mapPkg: make(map[string]*Pkg), depth: depth}
+}
+
+func (p *PkgTree) Add(name string, child string) {
+	pkg, ok := p.mapPkg[name]
+	if !ok {
+		pkg = &Pkg{name: name}
+		p.mapPkg[name] = pkg
+	}
+
+	pkg.childs = append(pkg.childs, child)
+
+	if len(p.root) == 0 {
+		p.root = name
+	}
+}
+
+func (p *PkgTree) GetPkg(name string) *Pkg {
+	return p.mapPkg[name]
+}
+
+func (p *PkgTree) GetRootPkg() *Pkg {
+	if len(p.root) == 0 {
+		return nil
+	}
+	return p.mapPkg[p.root]
+}
+
+func (p *PkgTree) printTree(name string, Arr1 []Artifact) int {
+
+	if count > p.depth {
+		count = 0
+		return -1
+	}
+	var s []Artifact
+	contains := strings.Contains(name, "/")
+	if contains {
+		res := strings.Split(name, "/")
+		st := Artifact{res[len(res)-2], res[len(res)-1], s}
+		Arr = append(Arr, st)
+	} else {
+		res := strings.Split(name, "@")
+		st := Artifact{res[0], res[1], s}
+		Arr = append(Arr, st)
+	}
+
+	child, ok := p.mapPkg[name]
+	if ok && len(child.childs) > 0 {
+		for _, name := range child.childs {
+			count = count + 1
+			if len(Arr) > 0 {
+				stparent := Arr[len(Arr)-1]
+				var s []Artifact
+				contains := strings.Contains(name, "/")
+				if contains {
+					res := strings.Split(name, "/")
+					st := Artifact{res[len(res)-2], res[len(res)-1], s}
+					stparent.Dependecies = append(stparent.Dependecies, st)
+				} else {
+					res := strings.Split(name, "@")
+					st := Artifact{res[0], res[1], s}
+					stparent.Dependecies = append(stparent.Dependecies, st)
+				}
+				Arr[len(Arr)-1].Dependecies = stparent.Dependecies
+				if p.printTree(name, stparent.Dependecies) == -1 {
+					break
+				}
+			}
+
+		}
+	}
+
+	return 0
+}
+
+var pDepth = flag.Int("d", 18, "max depth of dependence")
 
 func main() {
 
-	repoURL := "https://github.com/etcd-io/etcd"
-	clonePath := "D:\\Test"
-	Tag := "v3.5.5"
+	graphFile := "C:\\Users\\jeevan_b1\\Desktop\\Input.txt"
 
-	_, err := git.PlainClone(clonePath, false, &git.CloneOptions{
-		URL:               repoURL,
-		Progress:          os.Stdout,
-		ReferenceName:     plumbing.ReferenceName("refs/tags/" + Tag),
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
-	})
-	if err != nil {
-		panic(err)
+	var err error
+	var file *os.File
+	if len(graphFile) == 0 {
+		file = os.Stdin
+	} else {
+		file, err = os.Open(graphFile)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	println("cloned")
-	os.Chdir("D:\\Test")
-	out, err := exec.Command("go", "mod", "graph").Output()
+	reader := bufio.NewReader(file)
+	pkgTree := NewPkgTree(*pDepth)
 
-	if err != nil {
-		fmt.Printf("%s", err)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+
+		ss := strings.Split(string(line), " ")
+		if len(ss) != 2 {
+			if len(ss) == 1 {
+				pkgTree.Add(ss[0], ss[0])
+			} else {
+				log.Fatal(errors.New("error input"))
+			}
+		} else {
+			pkgTree.Add(ss[0], ss[1])
+		}
 	}
-	fmt.Println("Command Successfully Executed")
-	output := string(out[:])
-	fmt.Println(output)
+
+	root := pkgTree.GetRootPkg()
+	if root == nil {
+		return
+	}
+
+	fmt.Println("package:", root.name)
+	fmt.Println("dependence tree:\n")
+	for _, c := range root.childs {
+		pkgTree.printTree(c, Arr)
+	}
+
+	b, err := json.MarshalIndent(Arr, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
 }
